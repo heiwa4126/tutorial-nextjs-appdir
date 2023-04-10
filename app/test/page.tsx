893d8bf2d5923f37d6b6a71d85b550f63565d290
heiwa4126 <heiwa4126@gmail.com>
2023-04-10 16:53:58 +0900

    (snap)

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,5 +1,6 @@
 import UsersList from "./UsersList";
 import Counter from "./Countret";
+import { Suspense } from "react";
 
 const Test = async () => {
   return (
@@ -7,9 +8,12 @@ const Test = async () => {
       <h1 className="text-xl font-bold">Testページ</h1>
       <Counter />
       {/* Counterもサーバー側で静的に生成される。クライアント側でハイドレーションされcsrとなる */}
-      {/* @ts-expect-error Async Server Component */}
-      <UsersList />
-      {/* UsersListはbuild時に静的に生成されてしまう。devの時はリロードすると更新される */}
+
+      <Suspense fallback={<p className="mt-4">ユーザデータ Loading...</p>}>
+        {/* @ts-expect-error Async Server Component */}
+        <UsersList />
+        {/* UsersListはbuild時に静的に生成されてしまう。devの時はリロードすると更新される */}
+      </Suspense>
     </>
   );
 };
