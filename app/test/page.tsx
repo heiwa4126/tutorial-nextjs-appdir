dbc1783bc1668ecdc1bda8c5b21e3e4e6f0a53ae
heiwa4126 <heiwa4126@gmail.com>
2023-04-10 17:07:05 +0900

    (snap)

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,6 +1,7 @@
 import UsersList from "./UsersList";
 import Counter from "./Countret";
 import { Suspense } from "react";
+import PostsList from "./PostsList";
 
 const Test = async () => {
   return (
@@ -13,6 +14,10 @@ const Test = async () => {
         {/* @ts-expect-error Async Server Component */}
         <UsersList />
         {/* UsersListはbuild時に静的に生成されてしまう。devの時はリロードすると更新される */}
+      </Suspense>
+      <Suspense fallback={<p className="mt-4">記事データ Loading...</p>}>
+        {/* @ts-expect-error Async Server Component */}
+        <PostsList />
       </Suspense>
     </>
   );
